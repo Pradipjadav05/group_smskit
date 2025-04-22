e44c126171f3b696e524c85ae8ffd94d4bc1a792
pradip.j <pradip.j@rkitsoftware.com>
2025-04-22 11:44:28 +0530

    pub issue fixed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,5 @@
 ## 0.0.1
-
 * TODO: initial release.
+
+## 0.0.2
+TODO: pub issue fixed
--- lib/group_smskit_method_channel.dart
+++ lib/group_smskit_method_channel.dart
@@ -2,16 +2,15 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:group_smskit/group_smskit_platform_interface.dart';
 
-
 /// An implementation of [GroupSMSKitPlatform] that uses method channels.
 class MethodChannelGroupSMSKit extends GroupSMSKitPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
   final methodChannel = const MethodChannel('sms_sender');
 
-
   @override
-  Future<String?> sendSms({required List<String> numbers, required String message}) async {
+  Future<String?> sendSms(
+      {required List<String> numbers, required String message}) async {
     final result = await methodChannel.invokeMethod('sendSms', {
       'numbers': numbers,
       'message': message,
